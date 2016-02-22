ea9dddb4c40cac97f7cc9cdfd9be2994868efd57
jorge miranda <jorge miranda>
2016-02-21 21:12:48 -0300

    Atualização

--- src/main/java/br/pro/delfino/drograria/bean/CidadeBean.java
+++ src/main/java/br/pro/delfino/drograria/bean/CidadeBean.java
@@ -3,25 +3,53 @@ package br.pro.delfino.drograria.bean;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
+import org.omnifaces.util.Messages;
+
+import br.pro.delfino.drogaria.dao.CidadeDAO;
 import br.pro.delfino.drograria.domain.Cidade;
 
 @SuppressWarnings("serial")
 @ManagedBean
+@ViewScoped
 public class CidadeBean implements Serializable {
+	private Cidade cidade;
+	private List<Cidade> cidades;
 
-	List<Cidade> cidades;
-	
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
 	public List<Cidade> getCidades() {
 		return cidades;
 	}
+
 	public void setCidades(List<Cidade> cidades) {
 		this.cidades = cidades;
 	}
 
+	public void novo(){
+		cidade = new Cidade();
+	}
 	
-
+	@PostConstruct
+	public void listar() {
+		try {
+			CidadeDAO cidadeDAO = new CidadeDAO();
+			cidades = cidadeDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Erro ao listar as cidades");
+			erro.printStackTrace();
+		}
+	}
+	
 
 
 }
