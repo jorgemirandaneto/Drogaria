3c1ed5b63abe9ca2c4621cb420758fdc6b10ad02
jorge miranda <jorge miranda>
2016-03-01 07:43:40 -0300

    Atualização

--- src/main/java/br/pro/delfino/drogaria/bean/ProdutoBean.java
+++ src/main/java/br/pro/delfino/drogaria/bean/ProdutoBean.java
@@ -1,39 +1,92 @@
 package br.pro.delfino.drogaria.bean;
 
+import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.pro.delfino.drogaria.dao.FabricanteDAO;
 import br.pro.delfino.drogaria.dao.ProdutoDAO;
+import br.pro.delfino.drogaria.domain.Fabricante;
 import br.pro.delfino.drogaria.domain.Produto;
 
-public class ProdutoBean {
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class ProdutoBean implements Serializable {
+
 	Produto produto;
 	List<Produto> produtos;
+	List<Fabricante> fabricantes;
 	
-	public void setProduto(Produto produto) {
-		this.produto = produto;
-	}
-	
+
 	public Produto getProduto() {
 		return produto;
 	}
-	
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
 	}
-	
+
 	public List<Produto> getProdutos() {
 		return produtos;
 	}
-	
-	
-	public void listar(){
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	public void setFabricantes(List<Fabricante> fabricantes) {
+		this.fabricantes = fabricantes;
+	}
+
+	public List<Fabricante> getFabricantes() {
+		return fabricantes;
+	}
+
+	@PostConstruct
+	public void listar() {
 		try {
 			ProdutoDAO produtoDAO = new ProdutoDAO();
 			produtos = produtoDAO.listar();
 		} catch (Exception erro) {
 			erro.printStackTrace();
+			Messages.addGlobalError("Erro ao carregar lista");
 		}
-		
 	}
-	
+
+	public void novo() {
+		try {
+			produto = new Produto();
+
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricantes = fabricanteDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar gerar um novo produto");
+			erro.printStackTrace();
+		}
+	}
+
+	public void salvar() {
+		try {
+
+			ProdutoDAO produtoDAO = new ProdutoDAO();
+			produtos = produtoDAO.listar();
+
+			produto = new Produto();
+
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricantes = fabricanteDAO.listar();
+
+		} catch (Exception erro) {
+			erro.printStackTrace();
+			Messages.addGlobalError("Erro ao salvar produto");
+		}
+
+	}
+
 }
