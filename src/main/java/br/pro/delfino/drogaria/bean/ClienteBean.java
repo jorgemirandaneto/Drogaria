6b540ae9ae6c212890ad68fc0b974527c70d3572
jorge miranda <jorge miranda>
2016-04-02 18:58:40 -0300

    Atualização

--- src/main/java/br/pro/delfino/drogaria/bean/ClienteBean.java
+++ src/main/java/br/pro/delfino/drogaria/bean/ClienteBean.java
@@ -7,6 +7,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+
 import org.omnifaces.util.Messages;
 
 import br.pro.delfino.drogaria.dao.ClienteDAO;
@@ -20,35 +21,35 @@ import br.pro.delfino.drogaria.domain.Pessoa;
 public class ClienteBean implements Serializable {
 	private Cliente cliente;
 	private List<Pessoa> pessoas;
-	
+
 	private List<Cliente> clientes;
-	
+
 	public List<Cliente> getClientes() {
 		return clientes;
 	}
-	
+
 	public void setClientes(List<Cliente> clientes) {
 		this.clientes = clientes;
 	}
-	
+
 	public Cliente getCliente() {
 		return cliente;
 	}
-	
+
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
-	
+
 	public List<Pessoa> getPessoas() {
 		return pessoas;
 	}
-	
+
 	public void setPessoas(List<Pessoa> pessoas) {
 		this.pessoas = pessoas;
 	}
-	
+
 	@PostConstruct
-	public void listar(){
+	public void listar() {
 		try {
 			ClienteDAO clienteDAO = new ClienteDAO();
 			clientes = clienteDAO.listar();
@@ -56,20 +57,38 @@ public class ClienteBean implements Serializable {
 			erro.printStackTrace();
 			Messages.addFlashGlobalError("Erro ao listar clientes");
 		}
-		
-		
+
 	}
-	
-	public void novo(){
+
+	public void novo() {
 		try {
 			cliente = new Cliente();
-			
+
 			PessoaDAO pessoaDAO = new PessoaDAO();
 			pessoas = pessoaDAO.listar("nome");
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Erro ao gerar uma nova pessoa");
 			erro.printStackTrace();
 		}
-		
+
 	}
+
+	public void salvar() {
+		try {
+			ClienteDAO clienteDAO = new ClienteDAO();
+			clienteDAO.merge(cliente);
+
+			clientes = clienteDAO.listar();
+			
+			novo();
+			
+			Messages.addGlobalInfo("Cliente Salvo com sucesso");
+			
+			
+		} catch (RuntimeException erro) {
+			erro.printStackTrace();
+			Messages.addGlobalError("Erro ao salvar cliente");
+		}
+	}
+
 }
