0224e8c28224cf7e022ce41b9b00ee3410b160a9
jorge miranda <jorge miranda>
2016-07-12 23:51:29 -0300

    Atualização

--- src/main/java/br/pro/delfino/drogaria/bean/VendaBean.java
+++ src/main/java/br/pro/delfino/drogaria/bean/VendaBean.java
@@ -1,15 +1,18 @@
 package br.pro.delfino.drogaria.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
 import br.pro.delfino.drogaria.dao.ProdutoDAO;
+import br.pro.delfino.drogaria.domain.ItemVenda;
 import br.pro.delfino.drogaria.domain.Produto;
 
 @SuppressWarnings("serial")
@@ -17,7 +20,8 @@ import br.pro.delfino.drogaria.domain.Produto;
 @ViewScoped
 public class VendaBean implements Serializable {
 	
-	List<Produto> produtos;	
+	List<Produto> produtos;
+	List<ItemVenda> vendasItem;
 	
 	public List<Produto> getProdutos() {
 		return produtos;
@@ -27,16 +31,42 @@ public class VendaBean implements Serializable {
 		this.produtos = produtos;
 	}
 	
+	public List<ItemVenda> getVendasItem() {
+		return vendasItem;
+	}
+	
+	public void setVendasItem(List<ItemVenda> vendasItem) {
+		this.vendasItem = vendasItem;
+	}
+	
 	@PostConstruct
 	public void listar(){
 		try {
 			ProdutoDAO produtoDAO = new ProdutoDAO();
 			produtos = produtoDAO.listar("descricao");
+			
+			vendasItem = new ArrayList<>();
 		} catch (RuntimeException e) {
 			e.printStackTrace();
 			Messages.addGlobalError("Erro ao carregar produtos");
 		}
 		
 	}
+	
+	
+	public void adicionar(ActionEvent evento){
+		
+		Produto produto = (Produto) evento.getComponent().getAttributes().get("produtoSelecionado");
+		
+		ItemVenda itemVenda = new ItemVenda();
+		itemVenda.setValorParcial(produto.getPreco());
+		itemVenda.setProduto(produto);
+		itemVenda.setQuantidade(new Short("1"));
+		
+		vendasItem.add(itemVenda);
+		
+		System.out.println(produto);
+	}
+	
 		
 }
